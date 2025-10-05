a0621ee796b30ebb7af912ae8a1d34156c93ffd0
elparadoxo-sys <el.paradoxo@gmail.com>
2025-10-04 22:31:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -505,7 +505,7 @@ Thought: {agent_scratchpad}"""
         llm = ChatGroq(
             temperature=0,
             groq_api_key=groq_api_key,
-            model_name='meta-llama/llama-prompt-guard-2-86m'
+            model_name='meta-llama/llama-guard-4-12b'
         )
         
         agent = create_react_agent(llm, tools, prompt)
